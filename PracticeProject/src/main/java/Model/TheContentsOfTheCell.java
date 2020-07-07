cc386ac65f455489adbb5b224ee4674a74122783
KaterinaSyrtsova <katerinasyrcova@gmail.com>
2020-07-07 16:44:40 +0300

    Update TheContentsOfTheCell.java

--- PracticeProject/src/main/java/Model/TheContentsOfTheCell.java
+++ PracticeProject/src/main/java/Model/TheContentsOfTheCell.java
@@ -7,52 +7,128 @@ package Model;
  *
  * */
 
+import java.awt.*;
+
 //----------------------------- Класс содержимого клетки -----------------------------
 public abstract class TheContentsOfTheCell {
     private boolean g_isObstacle;//есть препятствие или нет
+    private TheContentsOfTheCell g_parent;
+    private Point g_location;//её расположение на сетке( Х и У )
+    private float g_costFromStart;//функция(g) - расстояние от начальной вершины
+    private float g_costToFinish;//функция(h) - оценка расстояния до финиша
 
-    public TheContentsOfTheCell(boolean isObstacle){
-        this.setObstacle(isObstacle);
+    public TheContentsOfTheCell(int x, int y, boolean isObstacle){
+        this.setObstacle(x, y, isObstacle);
     }
 
-    public void setObstacle(boolean isObstacle){
+    public void setObstacle(int x, int y, boolean isObstacle){
         this.g_isObstacle = isObstacle;
+        this.g_location.x = x;
+
+    }
+
+    public void set(TheContentsOfTheCell parent, TheContentsOfTheCell finish) {
+        float x;
+        float y;
+        if(parent != null){//если это НЕ стратовая вершина, то
+            //вычисляем расстояние от начальной вершины до текщей
+            this.g_costFromStart = this.calculateCostFromStart(parent);//устанавливаем функцию g(v)
+        }
+        if(finish != null){//если это НЕ стратовая вершина, то
+            //вычисляем функцию оценки до финиша!
+            x = this.g_location.x - finish.g_location.x;
+            y = this.g_location.y - finish.g_location.x;
+            this.g_costToFinish = (float) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+            //эвристическая оценка выбрана как евклидово расстояние
+            //может в дальнейшем изменить..?
+        }
+        this.g_parent = parent;
+    }
+
+    private float calculateCostFromStart(TheContentsOfTheCell parent){
+        float x = this.g_location.x - parent.g_location.x;
+        float y = this.g_location.y - parent.g_location.y;
+        float sum = (float) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        sum += this.getParent().getCostFromStart();
+        return sum;
+    }
+
+    public TheContentsOfTheCell getParent(){
+        return this.g_parent;
+    }
+
+    public boolean equals(TheContentsOfTheCell top){
+        return this.g_location.equals(top.getLocation());
+    }
+
+    public Point getLocation(){
+        return this.g_location;
+    }
+
+    public float getTotalCost(){
+        return this.g_costFromStart + this.g_costToFinish;
+    }
+
+    public float getCostFromStart(){
+        return this.g_costFromStart;
+    }
+
+    public float getCostToFinish(){
+        return this.g_costToFinish;
+    }
+
+    public String getParentMessage(){
+        if(this.g_parent == null){
+            return null;
+        }
+        return "[" + this.g_parent.getLocation().x + "," + this.g_parent.getLocation().y + "]";
+    }
+
+    public String getLocationMessage(){
+        return "[" + this.g_location.x + "," + this.g_location.y + "]";
+    }
+
+    public String toString(){
+        return "[" + this.g_location.x + "," + this.g_location.y + "]" + "\tparent: " + this.g_parent;
     }
 
     public boolean isObstacle(){
         return this.g_isObstacle;
     }
 
-
     public static class Empty extends TheContentsOfTheCell {
-        public Empty(){
-            super(false);//вызывается конструктор родительского класса
+        public Empty(int x, int y){
+            super(x , y, false);//вызывается конструктор родительского класса
             //МЫ ГОВОРИМ ЧТО КЛЕТКА ПУСТАЯ
         }
 
     }
 
+    public static class Close extends TheContentsOfTheCell {
+        public Close(int x, int y){
+            super(x, y,false);//вызывается конструктор родительского класса
+            //МЫ ГОВОРИМ ЧТО КЛЕТКА close
+        }
+
+    }
+
     public static class Block extends TheContentsOfTheCell {
-        public Block(){
-            super(true);
+        public Block(int x, int y){
+            super(x, y,true);
             // МЫ ГОВОРИМ ЧТО КЛЕТКА !НЕ! ПУСТАЯ - блок
         }
 
     }
 
     public static class Start extends TheContentsOfTheCell {
-        public Start(){
-            super(false);
+        public Start(int x, int y){
+            super(x, y,false);
         }
-
     }
 
     public static class Finish extends TheContentsOfTheCell {
-        public Finish(){
-            super(false);
+        public Finish(int x, int y){
+            super(x, y,false);
         }
-
     }
-
-
 }
